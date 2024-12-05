61e9b72cda539423ef4dc0032b2e2d358b61ee08
Sk93804 <sudheesh.zx@gmail.com>
2024-12-05 22:19:35 +0530

    update

--- Parameters.groovy
+++ Parameters.groovy
@@ -1,12 +1,12 @@
 pipeline{
-    agents any
+    agent any
     parameters{
       choice(name: 'Select the environment to run this job', choices: ['Dev', 'QA', 'Prod'])
     }
     stages{
         stage('checkout'){
            steps{ echo "this is code checkout stage"
-           }
+      }
         }
         stage('build'){
             steps{
