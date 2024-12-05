848783f50460c0dc25e05aef9feccdf54e6750f3
Sk93804 <sudheesh.zx@gmail.com>
2024-12-05 14:17:05 +0530

    update

--- Jenkins-when.groovy
+++ Jenkins-when.groovy
@@ -9,6 +9,7 @@ pipeline{
               git branch: 'main',
                     credentialsId: 'git',
                     url: 'https://github.com/Sk93804/Git-practice.git'
+              echo "checked out the code from the repository"
             }
         }
         stage("Build"){
