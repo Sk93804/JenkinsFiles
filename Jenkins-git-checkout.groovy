160c23a886ca79c2457251ea00bf8a6a0239407f
Sk93804 <sudheesh.zx@gmail.com>
2024-12-05 13:39:05 +0530

    Update

--- Jenkins-git-checkout.groovy
+++ Jenkins-git-checkout.groovy
@@ -1,25 +1,26 @@
-Pipeline{
+pipeline {
     agent any
-    environment{
+    environment {
         APP = 'Frontend'
         DB_URL = '192.64.17.1'
     }
-    stages{
-        stage('checkout'){
-            steps{
-             checkout scmGit(branches: [[name: '*/main']], 
-             extensions: [], 
-             userRemoteConfigs: [[credentialsId: 'git', url: 'https://github.com/Sk93804/Git-practice.git']])   
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main',
+                    credentialsId: 'git',
+                    url: 'https://github.com/Sk93804/Git-practice.git'
             }
         }
-        stage('Build'){
-            steps{
-            echo "Building the files..."
+        stage('Build') {
+            steps {
+                echo "Building the files..."
             }
         }
-        stage('Accessing the ENV variables')
-        steps{
-            echo "${env.APP}: ${env.DB_URL}"
+        stage('Accessing the ENV variables') {
+            steps {
+                echo "${env.APP}: ${env.DB_URL}"
+            }
         }
     }
 }
