eca53406186f898ae9deca1b236eb28fa5a51da4
Sk93804 <sudheesh.zx@gmail.com>
2024-12-05 22:16:00 +0530

    update

--- Parameters.groovy
+++ Parameters.groovy
@@ -1,29 +1,18 @@
-pipeline {
-    agent any
-    parameters {
-        string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
-
-        text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
-
-        booleanParam(name: 'TOGGLE', defaultValue: true, description: 'Toggle this value')
-
-        choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
-
-        password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+pipeline{
+    agents any
+    parameters{
+      choice(name: 'Select the environment to run this job', choices: ['Dev', 'QA', 'Prod'])
     }
-    stages {
-        stage('Example') {
-            steps {
-                echo "Hello ${params.PERSON}"
-
-                echo "Biography: ${params.BIOGRAPHY}"
-
-                echo "Toggle: ${params.TOGGLE}"
-
-                echo "Choice: ${params.CHOICE}"
-
-                echo "Password: ${params.PASSWORD}"
+    stages{
+        stage('checkout'){
+           steps{ echo "this is code checkout stage"
+           }
+        }
+        stage('build'){
+            steps{
+                echo "this is building stage"
             }
         }
     }
+
 }
