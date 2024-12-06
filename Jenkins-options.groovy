c244676c328e48a2825b08149d2e0a4d3bf5eba5
Sk93804 <sudheesh.zx@gmail.com>
2024-12-06 13:49:30 +0530

    commit

---
+++ Jenkins-options.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '5')) // Keep the logs of only the last 5 builds
+        retry(2) // Retry the failed stages up to 2 times
+        timeout(time: 1, unit: 'MINUTES') // Set a timeout of 1 minute for the pipeline
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                echo "Checkout stage"
+            }
+        }
+    }
+}
