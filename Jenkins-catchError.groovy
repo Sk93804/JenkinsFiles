87b710b21c62f7efdaeca73e10d7938381339058
Sk93804 <sudheesh.zx@gmail.com>
2024-12-09 09:08:41 +0530

    update

--- Jenkins-catchError.groovy
+++ Jenkins-catchError.groovy
@@ -3,15 +3,17 @@ pipeline{
     stages{
         stage('Checkout'){
             steps{
+            script{
               catchError(buildResult: 'FAILURE', stageResult: 'SUCCESS'){
               echo "This is code checkout stage"
               sh 'exit 1'
               }
             }
+            }
         }
         stage('build'){
             steps{
-            
+             echo "Building this stage irrespective of the errors "
             }
         }
     }
