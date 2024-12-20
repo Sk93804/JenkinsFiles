2ac4bab91b79ab6cc553fa380538641ff6f0e564
Sk93804 <sudheesh.zx@gmail.com>
2024-12-20 16:26:37 +0530

    update

---
+++ jenkins-maven.groovy
@@ -0,0 +1,23 @@
+pipeline{
+    agent {
+        label 'Slave1'
+    }
+    stages{
+        stage('SCM'){
+            steps{
+            checkout scmGit(branches: [[name: '*/main']], 
+            extensions: [], 
+            userRemoteConfigs: [[url: 'https://github.com/jabedhasan21/java-hello-world-with-maven.git']])
+    
+            }
+        }
+        stage('build'){
+            steps{
+                sh '''
+                cd  ./java-hello-world-with-maven.git 
+                mvn clean  package 
+                '''
+            }
+        }
+    }
+}
