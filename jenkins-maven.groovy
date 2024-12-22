b34845302eeb6cb16954efd8de5bdd05c2da36ae
Sk93804 <sudheesh.zx@gmail.com>
2024-12-22 09:57:14 +0530

    update

--- jenkins-maven.groovy
+++ jenkins-maven.groovy
@@ -2,10 +2,19 @@ pipeline{
     agent {
         label 'Slave1'
     }
+    environment{
+        GIT_BRANCH = 'main'
+        SC_URL = 'https://github.com/Azure-Samples/tomcat10-jakartaee9.git'
+        TOMCAT_URL = 'http://15.206.179.7:8080/manager/text'
+        WAR_FILE = 'home/ubuntu/tomcat10-jakartaee9/target/*.war'
+        CONTEXT_PATH = '/helloworld'
+        CREDENTIALS_ID = 'tomcat-credentials'
+
+    }
     stages{
         stage('SCM'){
             steps{
-            git branch: 'main', url: 'https://github.com/Azure-Samples/tomcat10-jakartaee9.git'
+            git branch: '${env.GIT_BRANCH}', url: '${env.SC_URL}'
     
             }
         }
@@ -17,8 +26,13 @@ pipeline{
         }
         stage('Deploy'){
             steps{
-                dir('target'){
-                    sh ' scp -r helloworld ubuntu@15.206.179.7:/home/ubuntu/tomcat10/webapps/ '
+                deploy adapters: [
+                    tomcat8(credentialsId: env.CREDENTIALS_ID, 
+                            path: '', 
+                            url: env.TOMCAT_URL)
+                ], 
+                contextPath: env.CONTEXT_PATH, 
+                war: env.WAR_FILE
                 }
             }
         }
