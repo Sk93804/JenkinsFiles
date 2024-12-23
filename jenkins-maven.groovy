1a2ba6d77753d288c6b8a8bcf637a6513f5d53a8
Sk93804 <sudheesh.zx@gmail.com>
2024-12-23 09:24:27 +0530

    update

--- jenkins-maven.groovy
+++ jenkins-maven.groovy
@@ -31,7 +31,7 @@ pipeline {
         stage('Build') {
             steps {
                 sh 'mvn clean package'
-                sh 'pwdupdate'
+                sh 'pwd'
             }
         }
         stage('Deploy') {
