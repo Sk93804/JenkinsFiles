fffd3505689bb89d66f818360d1fde4ff35c376b
Sk93804 <sudheesh.zx@gmail.com>
2025-01-02 12:42:44 +0530

    update

--- jenkins-webhook.groovy
+++ jenkins-webhook.groovy
@@ -10,7 +10,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                build(goal: 'clean', name: 'package') // Pass the required parameter
+                customBuild(goal: 'clean', name: 'package') // Pass the required parameter
             }
         }
     }
