fc13d6e06c95fbcf9d10067c6f063b982999a33c
Sk93804 <sudheesh.zx@gmail.com>
2025-01-03 09:44:24 +0530

    upda

--- jenkins-webhook.groovy
+++ jenkins-webhook.groovy
@@ -5,7 +5,7 @@ pipeline {
         stage('SCM') {
             steps {
                 script{
-                    def config = [
+                    Map config = [
                         url: 'https://github.com/Sk93804/Maven-tomcat.git'
                         branch: 'main'
                     ]
